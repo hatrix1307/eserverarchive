da81103e31cb2ac61fb4b2e0fccf52b35110fdab
hatrix1307 <145073127+hatrix1307@users.noreply.github.com>
2025-12-28 20:41:27 -0500

    Update [server].js

--- api/status/[server].js
+++ api/status/[server].js
@@ -1,11 +1,17 @@
 export default async function handler(req, res) {
   const { server } = req.query;
 
+  if (!server) {
+    res.status(400).json({ success: false, error: "Server parameter missing" });
+    return;
+  }
+
   try {
     const r = await fetch(`https://topeaglerservers.com/api/status/${server}`);
     const data = await r.json();
+
     res.status(200).json(data);
-  } catch (e) {
-    res.status(500).json({ success: false, error: e.message });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
   }
 }
